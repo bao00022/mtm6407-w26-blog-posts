e26a2da0f4551bf8e3e803804362ebbfd30c8b52
bao00022 <bao00022@algonquinlive.com>
2026-02-24 21:44:45 -0500

    completed

--- src/components/BlogPost.vue
+++ src/components/BlogPost.vue
@@ -1,50 +1,56 @@
 <script setup>
-
 // Define the 'post' prop (Hint: the prop type should be Object)
-
+defineProps({
+  post: {
+    id: Number,
+    author: String,
+    publishedAt: String,
+    title: String,
+    image: String,
+    content: String,
+  },
+});
 </script>
 
 <template>
-
-    <span>
-        <img :src="post.image" :alt="post.title" />
-        <h1>{{ post.title }}</h1>
-        <p>By: {{ post.author }}</p>
-        <p>Published: {{ post.publishedAt }}</p>
-        <div v-html="post.content"></div>
-    </span>
-
+  <span>
+    <img :src="post.image" :alt="post.title" />
+    <h1>{{ post.title }}</h1>
+    <p>By: {{ post.author }}</p>
+    <p>Published: {{ post.publishedAt }}</p>
+    <div v-html="post.content"></div>
+  </span>
 </template>
 
 <style scoped>
-    span {
-        display: block;
-        padding: 10px;
-        border: 1px solid #DDD;
-        border-radius: 6px;
-        width: 100%;
-    }
+span {
+  display: block;
+  padding: 10px;
+  border: 1px solid #ddd;
+  border-radius: 6px;
+  width: 100%;
+}
 
-    h1 {
-        color: #111;
-        font-size: 24px;
-        margin: 12px 0;
-    }
+h1 {
+  color: #111;
+  font-size: 24px;
+  margin: 12px 0;
+}
 
-    h2 {
-        color: #111;
-        font-size: 18px;
-        margin: 12px 0;
-    }
+h2 {
+  color: #111;
+  font-size: 18px;
+  margin: 12px 0;
+}
 
-    p {
-        font-size: 12px;
-        margin: 6px 0;
-    }
+p {
+  font-size: 12px;
+  margin: 6px 0;
+}
 
-    img {
-        display: block;
-        max-width: 100%;
-        height: auto;
-    }
+img {
+  display: block;
+  max-width: 100%;
+  height: auto;
+}
 </style>
--- src/components/BlogPosts.vue
+++ src/components/BlogPosts.vue
@@ -1,33 +1,32 @@
 <script setup>
-
 // Import the 'BlogPost' component
-import { postData } from '@/post-data';
-import BlogPost from './BlogPost.vue';
+import { postData } from "@/post-data";
+import BlogPost from "./BlogPost.vue";
 
- // Define the 'posts' prop (Hint: the prop type should be Array)
+// Define the 'posts' prop (Hint: the prop type should be Array)
 
-
+defineProps({
+  posts: Array,
+});
 </script>
 
 <template>
-    <ul class="blog-posts">
-        <li v-for="post in posts" :key="post.id">
-            <BlogPost :post="post" />
-        </li>
-    </ul>
+  <ul class="blog-posts">
+    <li v-for="post in posts" :key="post.id">
+      <BlogPost :post="post" />
+    </li>
+  </ul>
 </template>
 
 <style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
 
-    ul {
-        list-style: none;
-        margin: 0;
-        padding: 0;
-    }
-
-    li {
-        margin: 0 0 30px 0;
-        padding: 0;
-    }
-
+li {
+  margin: 0 0 30px 0;
+  padding: 0;
+}
 </style>
